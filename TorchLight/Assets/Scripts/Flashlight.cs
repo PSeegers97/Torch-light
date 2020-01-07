213d4338af4b4ca3742072a5a8ac7ff9fc2c6399
Johnybohny <45261157+Johnybohny@users.noreply.github.com>
2020-01-07 22:51:44 +0100

    Clean Install with Movement and Flashlight

---
+++ TorchLight/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private bool flashlightEnabled=true;
+    
+    public GameObject mylight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad).x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.Back))
+        {
+             
+            flashlightEnabled =  !flashlightEnabled;
+            
+        
+        if (flashlightEnabled==true ) {
+                mylight.SetActive(true);
+               
+        }
+        else if(flashlightEnabled==false)
+        {
+                mylight.SetActive(false);
+        }
+        }
+    }
+}
+
---
+++ TorchLight/Assets/Scripts/Move2me.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2me : MonoBehaviour
+{
+    public Transform myParent;
+    private Transform trans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = gameObject.transform;
+       // Debug.Log("Test");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position.Set(myParent.position.x+90, myParent.position.y, myParent.position.z);
+     
+    }
+}
