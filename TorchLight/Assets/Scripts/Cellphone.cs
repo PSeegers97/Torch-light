f9a4aadb6fc1257d4e900ed9cf3af8cd0ea7808b
Sebastian Mertins <sebastian@sem-ba.de>
2020-01-09 21:00:25 +0100

    Improved sound for cellphone

---
+++ TorchLight/Assets/Scripts/Cellphone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cellphone : MonoBehaviour
+{
+    public float time = 5.0f;
+    public AudioClip audioClip;
+
+    private bool active;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            Debug.Log("Actual time: "+time);
+        }
+        else
+        {
+            if (!active)
+            {
+                gameObject.GetComponent<AudioSource>().loop = true;
+                gameObject.GetComponent<AudioSource>().Play();
+                Debug.Log("Ring");
+                active = true;
+            }
+            
+        }
+
+    }
+
+    IEnumerator playRing()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+        
+        yield return new WaitForSeconds(2);
+    }
+}
